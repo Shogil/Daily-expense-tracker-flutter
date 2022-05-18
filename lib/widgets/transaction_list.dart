69222d762319e381716564ddf52c672da44b2ab3
shogil <shogilbabu@gmail.com>
2022-05-18 15:58:21 +0530

    update the transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,53 +9,51 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '₹${tx.amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
+      child: Column(
+        children: transactions.map((tx) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
                   ),
-                  Column(children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '₹${tx.amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
+                  ),
+                ),
+                Column(children: <Widget>[
+                  Text(
+                    tx.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ])
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(tx.date),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ])
+              ],
+            ),
+          );
+        }).toList(),
       ),
     );
   }

f92cb5bbe057041772b7698c1256b2731a712270
shogil <shogilbabu@gmail.com>
2022-05-24 19:45:53 +0530

    update the transaction file

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sort Out',
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: MyHomePage(),
     );
   }

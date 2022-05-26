b23a4812ec7b4eeb43f1defd7bbaffdb9c5b9863
shogil <shogilbabu@gmail.com>
2022-05-26 18:56:23 +0530

    update the image file

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,6 @@
 import 'package:daily_expense_tracker_flutter/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -8,7 +9,15 @@ class Chart extends StatelessWidget {
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
-      return {'day': 'T', 'amount': 0};
+      double totalSum = 0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      return {'day': DateFormat.E(weekDay), 'amount': 9.99};
     });
   }
 

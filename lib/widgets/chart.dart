e12e20d9c08be70a4b9a8a16330df8d0be226c63
shogil <shogilbabu@gmail.com>
2022-06-01 18:47:19 +0530

    update the chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -21,7 +21,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weeekday).substring(0, 1),
         'amount': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {

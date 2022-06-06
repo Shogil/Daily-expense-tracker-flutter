9deabe0cb1b6edda5803c00910357956450f98cb
shogil <shogilbabu@gmail.com>
2022-06-06 16:16:13 +0530

    update the list

--- lib/main.dart
+++ lib/main.dart
@@ -123,12 +123,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
-                    0.4,
+                    0.3,
                 child: Chart(_recentTransactions)),
             Container(
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height) *
-                    0.6,
+                    0.7,
                 child: TransactionList(_userTransaction, _deleteTransaction)),
           ],
         ),
